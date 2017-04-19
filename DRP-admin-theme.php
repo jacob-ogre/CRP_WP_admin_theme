4b00fda566a23efa3557c2b6e67cfb8cdaaa2171
Jacob Malcom <jmalcom@defenders.org>
2017-04-19 16:43:32 -0400

    Initial commit.

---
+++ DRP-admin-theme.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Plugin Name: Dynamic Recovery Plan (DRP) Admin Theme
+ * Plugin URI: https://github.com/jacob-ogre/CRP_WP_admin_theme
+ * Description: Clean, eye-pleasing theme
+ * Author: jacob-ogre
+ * Version: 1.0
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * */
+
+function material_style() {
+        wp_enqueue_style('material-theme', plugins_url('material.css', __FILE__));
+}
+
+add_action('admin_enqueue_scripts', 'material_style');
+add_action('login_enqueue_scripts', 'material_style');
+
+?>
+
